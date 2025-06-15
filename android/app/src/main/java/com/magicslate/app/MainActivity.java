6e34eff16379a950905a89f947b8536f9f0bd834
Ayush2006128 <ayush.muley1907@outlook.com>
2025-06-15 07:31:53 +0000

    Remove Capacitor dependencies and add a new icon for the manifest

--- android/app/src/main/java/com/magicslate/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.magicslate.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.magicslate.app',
-  appName: 'Magic Slate',
-  webDir: 'out'
-};
-
-export default config;
